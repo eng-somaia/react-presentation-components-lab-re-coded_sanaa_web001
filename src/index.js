e85bd4493eef786c80d99e593196b61ecf961d06
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 12:29:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
-  
+
   let clickHandle = () => console.log('Clicked')
 
 ReactDOM.render(
